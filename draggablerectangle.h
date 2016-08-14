b4b8e5835008b751ba2aa311ce75bff563451642
uveavanto <yuv3sh@gmail.com>
2016-08-14 19:17:49 +0100

    temp fix to code so it runs properly (commented out unimplememnted functions)

--- draggablerectangle.h
+++ draggablerectangle.h
@@ -1,22 +1,24 @@
 #pragma once
 
 //#include "ofApp.h"
-#include <ofApp.h>
+#include "ofApp.h"
 
 class draggablerectangle
 {
+	
 protected:
 	ofRectangle* draggableBounds;
-	bool isBeingDragged;
+	//bool isBeingDragged;
 
-	bool isHovered(float x, float y);
+	//bool isHovered(float x, float y);
 
 public:
 	draggablerectangle();
 	~draggablerectangle();
 
-	virtual void draw();
-	bool mouseDragged(float x, float y, int button);
-	bool mousePressed(float x, float y, int button);
-};
+	//virtual void draw();
+	//bool mouseDragged(float x, float y, int button);
+	//bool mousePressed(float x, float y, int button);
+	
+	};
 
